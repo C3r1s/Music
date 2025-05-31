d7a6ae329c10d5df670a06090ffc80c3bf3fbf0e
Artyom Merekin <145928948+C3r1s@users.noreply.github.com>
2025-05-31 19:35:33 +0300

    Implemented the search function

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Music.Data.Repositories;
+using Music.Models;
 
 namespace Music.Controllers;
 
@@ -9,7 +10,39 @@ public class HomeController(MusicDbContext context) : Controller
     public async Task<IActionResult> Index()
     {
         var artists = await context.Artists.AsNoTracking().ToListAsync();
-
         return View(artists);
     }
+
+    public async Task<IActionResult> Search(string query)
+    {
+        if (string.IsNullOrWhiteSpace(query))
+            return RedirectToAction(nameof(Index));
+
+        var artists = await context.Artists
+            .Where(a => a.Name.Contains(query))
+            .AsNoTracking()
+            .ToListAsync();
+
+        var albums = await context.Albums
+            .Where(a => a.Name.Contains(query))
+            .AsNoTracking()
+            .Include(a => a.Songs)
+            .ToListAsync();
+
+        var songs = await context.Songs
+            .Where(s => s.Name.Contains(query))
+            .AsNoTracking()
+            .ToListAsync();
+
+        var model = new SearchViewModel
+        {
+            Query = query,
+            Artists = artists,
+            Albums = albums,
+            Songs = songs
+        };
+
+        return View(model);
+    }
 }
+
---
+++ Models/SearchViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Music.Models;
+
+public class SearchViewModel
+{
+    public string Query { get; set; } = string.Empty;
+    public List<Artist> Artists { get; set; } = [];
+    public List<Album> Albums { get; set; } = [];
+    public List<Song> Songs { get; set; } = [];
+}
